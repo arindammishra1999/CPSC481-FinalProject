7d81842c0286d120cf2a1ca5c45bbba8deffd8bb
arindammishra1999 <arimishra1999@gmail.com>
2024-04-06 19:17:35 -0600

    Fix some recipe styling

--- Frontend/app/chocolatechip.js
+++ Frontend/app/chocolatechip.js
@@ -2,13 +2,15 @@ let quantity = 1;
 const quantityInput = document.getElementById("quantity");
 const decreaseButton = document.getElementById("decrease-btn");
 const increaseButton = document.getElementById("increase-btn");
-const checkboxes = document.querySelectorAll('.checkbox input[type="checkbox"]');
+const checkboxes = document.querySelectorAll(
+    '.checkbox input[type="checkbox"]'
+);
 const nextButton = document.getElementById("next-btn");
 const previousButton = document.getElementById("prev-btn");
 
 function updateButtonsState() {
-    decreaseButton.disabled = (quantity == 0.25); 
-    increaseButton.disabled = (quantity == 10);
+    decreaseButton.disabled = quantity == 0.25;
+    increaseButton.disabled = quantity == 10;
 }
 
 decreaseButton.addEventListener("click", () => {
@@ -29,13 +31,13 @@ increaseButton.addEventListener("click", () => {
     updateIngredientQuantities();
 });
 
-checkboxes.forEach(checkbox => {
+checkboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", checkCheckboxes);
 });
 
 function checkCheckboxes() {
     let allChecked = true;
-    checkboxes.forEach(checkbox => {
+    checkboxes.forEach((checkbox) => {
         if (!checkbox.checked) {
             allChecked = false;
         }
@@ -44,7 +46,7 @@ function checkCheckboxes() {
 }
 
 nextButton.addEventListener("click", () => {
-    window.location.href = "recipe.html";
+    window.location.href = "chocolateChipRecipe.html";
 });
 
 previousButton.addEventListener("click", () => {
@@ -62,17 +64,17 @@ function updateIngredientQuantities() {
         1, // baking soda
         0.5, // baking powder
         1, // sea salt
-        2 // chocolate chips
+        2, // chocolate chips
     ];
-        
-    const ingredients = document.querySelectorAll('.checkbox');
+
+    const ingredients = document.querySelectorAll(".checkbox");
     ingredients.forEach((ingredient, index) => {
-        const quantitySpan = ingredient.querySelector('span');
+        const quantitySpan = ingredient.querySelector("span");
         quantitySpan.innerText = ingredientQuantities[index] * quantity;
     });
 
-    const total = document.getElementById('total');
-    total.innerText = (36 * quantity) + ' Cookies';
+    const total = document.getElementById("total");
+    total.innerText = 36 * quantity + " Cookies";
 }
 
 // Initial call to update ingredient quantities
